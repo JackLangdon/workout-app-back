2e0de4dbc1bc25e9254d0190a5898b87cecd575d
John <john@steadlane.com.au>
2022-04-16 10:00:42 +1000

    added breeze, sanctum, added first_name and last_name to user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'user@test.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('abcd1234!'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        \App\Models\User::factory(10)->create();
     }
 }
